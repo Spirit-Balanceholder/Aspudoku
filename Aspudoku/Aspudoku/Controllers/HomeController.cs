d7f360a515a1197ceb7d956d8d55e319272e26ab
Spirit-Balanceholder <martinkleinpaste@hotmail.com>
2015-01-15 19:10:46 +0100

    aspudoku

---
+++ Aspudoku/Aspudoku/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Aspudoku.Models;
+using Sudoku;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aspudoku.Controllers
+{
+    public class HomeController : Controller
+    {
+        Sudo sudo = new Sudo();
+        //
+        // GET: /Home/
+        public ActionResult Index(int CreateGame = 0)
+        {
+            if (CreateGame == 1)
+            {
+                sudo.Create();
+                ViewData["puzzle"] = sudo.puzzle;
+            }
+            return View();
+        }
+    }
+}
---
+++ Aspudoku/Aspudoku/Models/Sudo.cs
@@ -0,0 +1,30 @@
+﻿using Sudoku;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aspudoku.Models
+{
+    public class Sudo
+    {
+        Game gm = new Game();
+        public short[,] puzzle { get; set; }
+        public Sudo()
+        {
+            puzzle = new short[9, 9];
+        }
+
+        public void Create ()
+        {
+            gm.create();
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    gm.get((short)(x + 1), (short)(y + 1), out puzzle[x, y]);
+                }
+            }
+        }
+    }
+}
